f62de0ef2b4b37ca5a215f282d34f03bf4dccd3e
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-07-30 16:02:36 -0300

    feat: route navigation strutuctured & pages added

---
+++ src/route.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes
+} from "react-router-dom";
+
+import Home from "./pages/home";
+import Login from "./pages/login";
+
+const RouteNavigation = () => {
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/login" element={<Login />} />
+                </Routes>
+            </Router>
+        </>
+    );
+};
+
+
+export default RouteNavigation;
